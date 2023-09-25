ab77c409b6066d11cbca06e7710d0d5a87c70dfb
Cody <domorin821@gmail.com>
2023-09-25 16:57:29 -0700

    disable shader if GPU tier sucks

--- src/app/_components/shader.tsx
+++ src/app/_components/shader.tsx
@@ -8,12 +8,26 @@ import {
 	useShaderSettings,
 } from "../_hooks/use_shader_settings";
 import { ShaderDropdown } from "./shader_dropdown";
+import { getGPUTier } from "detect-gpu";
 
 function setUniform(ref: MutableRefObject<any>, settings: ShaderSettings) {
 	ref.current?.setUniform("u_speed", settings.speed);
 	ref.current?.setUniform("u_brightness", settings.brightness);
 	ref.current?.setUniform("u_zoom", settings.zoom);
 	ref.current?.setUniform("u_thickness_factor", settings.lineThickness);
+}
+
+function hasWebGLSupport() {
+	try {
+		var canvas = document.createElement("canvas");
+		return (
+			!!window.WebGLRenderingContext &&
+			(canvas.getContext("webgl") ||
+				canvas.getContext("experimental-webgl"))
+		);
+	} catch (e) {
+		return false;
+	}
 }
 
 export function Shader() {
@@ -26,21 +40,40 @@ export function Shader() {
 	} as ShaderSettings);
 
 	const sandboxRef = useRef<any>(null);
+	const hasInitialUseEffectRun = useRef(false);
 
 	useEffect(() => {
-		const glslCanvas = require("glslCanvas");
+		if (!shaderSettings.enabled) {
+			return;
+		}
 
-		const canvas = document.createElement("canvas");
-		canvas.style.width = "100vw";
-		canvas.style.height = "100vh";
+		getGPUTier()
+			.then((val) => {
+				console.log(val);
+				// If this is not in the initial useEffect, user is opting in to enable the shader; let them
+				if (val.tier < 2 && !hasInitialUseEffectRun.current) {
+					console.log("Hello?");
+					setShaderSettings((prev) => ({ ...prev, enabled: 0 }));
+					return;
+				}
 
-		sandboxRef.current = new glslCanvas.default(canvas);
+				const glslCanvas = require("glslCanvas");
 
-		sandboxRef.current.load(test[0]);
+				const canvas = document.createElement("canvas");
+				canvas.style.width = "100vw";
+				canvas.style.height = "100vh";
 
-		setUniform(sandboxRef, shaderSettings);
+				sandboxRef.current = new glslCanvas.default(canvas);
 
-		document.getElementById("shader-parent")?.replaceChildren(canvas);
+				sandboxRef.current.load(test[0]);
+
+				setUniform(sandboxRef, shaderSettings);
+
+				document
+					.getElementById("shader-parent")
+					?.replaceChildren(canvas);
+			})
+			.finally(() => (hasInitialUseEffectRun.current = true));
 	}, [shaderSettings.enabled]);
 
 	setUniform(sandboxRef, shaderSettings);
